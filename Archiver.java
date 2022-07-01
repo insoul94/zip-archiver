72ba8a856e88c7afa6cdead9406c23a9d6677a35
Jevgeni <belan.evgenii@gmail.com>
2022-07-01 20:12:36 +0300

    Added askOperation(), choose operation from console.

--- Archiver.java
+++ Archiver.java
@@ -1,21 +1,38 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
+    }
 
-        System.out.println("Enter new archive full path:");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
-
-        System.out.println("Enter full path of the file to be archived:");
-        zipFileManager.createZip(Paths.get(bis.readLine()));
-
-        new ExitCommand().execute();
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage(String.format("\nВыберите операцию\n" +
+                        "\t %d - упаковать файлы в архив\n" +
+                        "\t %d - добавить файл в архив\n" +
+                        "\t %d - удалить файл из архива\n" +
+                        "\t %d - распаковать архив\n" +
+                        "\t %d - просмотреть содержимое архива\n" +
+                        "\t %d - выход\n",
+                Operation.CREATE.ordinal(),
+                Operation.ADD.ordinal(),
+                Operation.REMOVE.ordinal(),
+                Operation.EXTRACT.ordinal(),
+                Operation.CONTENT.ordinal(),
+                Operation.EXIT.ordinal()));
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
