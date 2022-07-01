862b755a6ef1ebc3021908b91d30b75e3b278a61
Jevgeni <belan.evgenii@gmail.com>
2022-07-01 17:40:54 +0300

    Created interface Command, Exit Command class

--- Archiver.java
+++ Archiver.java
@@ -1,26 +1,21 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
-            System.out.println("Enter archive full path:");
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
+        System.out.println("Enter new archive full path:");
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
 
-            System.out.println("Enter full path of file to be archived:");
-            zipFileManager.createZip(Paths.get(bis.readLine()));
+        System.out.println("Enter full path of the file to be archived:");
+        zipFileManager.createZip(Paths.get(bis.readLine()));
 
-        } catch (IOException e) {
-            System.out.println("Exception while reading file path: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception creating ZIP: " + e.getMessage());
-        }
-
+        new ExitCommand().execute();
     }
 }
