021c0a43413772463f7aa77a26ad0eed1d04b0aa
Jevgeni <belan.evgenii@gmail.com>
2022-07-02 17:25:55 +0300

    Impelented: remove file from archive

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -68,9 +68,9 @@ public class ZipFileManager {
                 Files.createDirectories(outputFolder);
 
             // Проходимся по содержимому zip потока (файла)
-            ZipEntry zipEntry;
-            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
 
+            while (zipEntry != null) {
                 String fileName = zipEntry.getName();
                 Path fileFullName = outputFolder.resolve(fileName);
 
@@ -82,8 +82,50 @@ public class ZipFileManager {
                 try (OutputStream outputStream = Files.newOutputStream(fileFullName)) {
                     copyData(zipInputStream, outputStream);
                 }
+                zipEntry = zipInputStream.getNextEntry();
             }
         }
+    }
+
+    public void removeFile(Path path) throws Exception {
+        removeFiles(Collections.singletonList(path));
+    }
+
+    public void removeFiles(List<Path> pathList) throws Exception {
+        // Проверяем существует ли zip файл
+        if (!Files.isRegularFile(zipFile)) {
+            throw new WrongZipFileException();
+        }
+
+        // Создаем временный файл
+        Path tempZipFile = Files.createTempFile(null, null);
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempZipFile))) {
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+
+                ZipEntry zipEntry = zipInputStream.getNextEntry();
+                while (zipEntry != null) {
+
+                    Path archivedFile = Paths.get(zipEntry.getName());
+
+                    if (!pathList.contains(archivedFile)) {
+                        String fileName = zipEntry.getName();
+                        zipOutputStream.putNextEntry(new ZipEntry(fileName));
+
+                        copyData(zipInputStream, zipOutputStream);
+
+                        zipOutputStream.closeEntry();
+                        zipInputStream.closeEntry();
+                    } else {
+                        ConsoleHelper.writeMessage(String.format("Файл '%s' удален из архива.", archivedFile.toString()));
+                    }
+                    zipEntry = zipInputStream.getNextEntry();
+                }
+            }
+        }
+
+        // Перемещаем временный файл на место оригинального
+        Files.move(tempZipFile, zipFile, StandardCopyOption.REPLACE_EXISTING);
     }
 
     public List<FileProperties> getFilesList() throws Exception {
