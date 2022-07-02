199c312a0dfd855b8ee4f382fadb4affb9e1f761
Jevgeni <belan.evgenii@gmail.com>
2022-07-02 11:13:38 +0300

    Added FileProperties class

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -2,15 +2,15 @@ package com.javarush.task.task31.task3110.command;
 
 import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.ZipFileManager;
-import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
+public abstract class ZipCommand implements Command {
+
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь архива:");
-        Path zipFullPath = Paths.get(ConsoleHelper.readString());
-        return new ZipFileManager(zipFullPath);
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
