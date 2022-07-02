1055451dd616f57f2e797d8dab586a912ee2aae1
Jevgeni <belan.evgenii@gmail.com>
2022-07-02 10:54:05 +0300

    Implement ZipCreateCommand class; getZipFileManager() in ZipCommand

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -1,4 +1,16 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь архива:");
+        Path zipFullPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipFullPath);
+    }
 }
--- command/ZipCreateCommand.java
+++ command/ZipCreateCommand.java
@@ -1,8 +1,27 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand{
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Создание архива.");
 
+        try {
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полный путь архивируемого файла/папки:");
+            Path sourceFullPath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.createZip(sourceFullPath);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
